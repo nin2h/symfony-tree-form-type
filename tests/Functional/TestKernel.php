2d1c5e9a1d83ede78d3f2037a6a6d727dfd7508b
Lightman <yaruk.my@gmail.com>
2021-01-02 19:10:39 +0200

    create TestKernel for using in tests

---
+++ tests/Functional/TestKernel.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\TreeTypeBundle\Tests\Functional;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Mrself\TreeTypeBundle\TreeTypeBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new DoctrineBundle(),
+            new TreeTypeBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/services.yaml');
+    }
+
+}
