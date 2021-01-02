7b72d3c448cd68ed8741cdf2f76eb80a82f86ce7
Lightman <yaruk.my@gmail.com>
2021-01-02 16:33:12 +0200

    fix: make MrTreeTypeExtension according to symfony bundle convention

--- src/DependencyInjection/MrTreeTypeExtension.php
+++ src/DependencyInjection/MrTreeTypeExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class TreeTypeExtension extends Extension
+class MrTreeTypeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
