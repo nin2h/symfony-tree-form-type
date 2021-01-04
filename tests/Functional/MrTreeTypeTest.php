80726084e4c803d6baf79b2796e33bec4a75f151
Lightman <yaruk.my@gmail.com>
2021-01-04 08:32:00 +0200

    add testItAddsSelectedStateToTreeItemsWhichAreSelected

--- tests/Functional/MrTreeTypeTest.php
+++ tests/Functional/MrTreeTypeTest.php
@@ -2,6 +2,7 @@
 
 namespace Mrself\TreeTypeBundle\Tests\Functional;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Mrself\TreeTypeBundle\MrTreeType;
 use Symfony\Component\Form\FormFactory;
 use Twig\Environment;
@@ -29,6 +30,36 @@ class MrTreeTypeTest extends KernelTestCase
         $mrTreeTypeBlockName = 'mrTreeWidget';
         $this->assertStringContainsString($mrTreeTypeBlockName, $html);
         $this->assertStringContainsString('Item 1', $html);
+    }
+
+    public function testItAddsSelectedStateToTreeItemsWhichAreSelected()
+    {
+        $entity = new class {
+            public function getId()
+            {
+                return 1;
+            }
+        };
+        $collection = new ArrayCollection([$entity]);
+
+        /** @var FormFactory $formFactory */
+        $formFactory = static::$container->get('form.factory');
+        $form = $formFactory->createBuilder(MrTreeType::class, $collection, [
+            'tree' => [
+                [
+                    'id' => 1,
+                    'parent' => '#',
+                    'text' => 'Item 1'
+                ]
+            ],
+            'multiple' => true,
+        ]);
+        $view = $form->getForm()->createView();
+        /** @var Environment $twig */
+        $twig = static::$container->get('twig');
+        $html = $twig->createTemplate('{{ form(form) }}')->render(['form' => $view]);
+
+        $this->assertStringContainsString('"selected"', $html);
     }
 
     protected static function getKernelClass()
