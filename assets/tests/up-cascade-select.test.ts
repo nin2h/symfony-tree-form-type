e1e3275e75fa5a098b08919305f1675919533960
Lightman <yaruk.my@gmail.com>
2021-01-30 15:16:02 +0200

    test: it ignores parent parents which can not be selected on node select

--- assets/tests/up-cascade-select.test.ts
+++ assets/tests/up-cascade-select.test.ts
@@ -33,3 +33,37 @@ test('it selected parents on node select', cb => {
         }
     });
 });
+
+test('it ignores parent parents which can not be selected on node select', cb => {
+    const testWidget = TestWidget.init({
+        globalOptions: {
+            data: [
+                {
+                    id: 1,
+                    parent: '#',
+                    text: 'Item 1',
+                    canBeSelected: false
+                },
+                {
+                    id: 2,
+                    parent: '1',
+                    text: 'Item 2',
+                },
+                {
+                    id: 3,
+                    parent: '2',
+                    text: 'Item 3',
+                }
+            ]
+        },
+        jstreeOptions: {
+            upCascadeSelect: true
+        },
+        callback: (instance: MrTreeWidget) => {
+            instance.getJstree().select_node(3);
+            const value = testWidget.$input.val();
+            expect(value).toEqual('3,2');
+            cb();
+        }
+    });
+});
